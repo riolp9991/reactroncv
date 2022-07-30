262cf9a25e81be890c28d6b43a4bb61300cacd48
riolp <riolp9991@gmail.com>
2022-07-30 17:14:38 -0400

    feat: add material UI

--- src/components/Profiles/Profiles.jsx
+++ src/components/Profiles/Profiles.jsx
@@ -2,10 +2,10 @@ import {Link} from 'react-router-dom'
 
 const Profiles = () => {
   return (
-    <div>
-      <h1>Profiles</h1>
+    <div style={{textAlign: "left", padding: '10px'}}>
+      <h1 style={{margin: 0, marginBottom: '10px', color: '#EB4D4D', fontSize: '1.3rem'}}>Profiles</h1>
       <Link className='home-btn' to='/'>Home</Link>
-    </div>
+    </div >
   )
 }
 
