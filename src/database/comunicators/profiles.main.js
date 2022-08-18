4ce36cdd60eabcd0fe7cd13a31080a9e1a3eae8f
riolp <riolp9991@gmail.com>
2022-08-17 21:06:11 -0400

    something a long ago

--- src/database/comunicators/profiles.main.js
+++ src/database/comunicators/profiles.main.js
@@ -1,15 +1,15 @@
-const {ipcMain} = require('electron');
-const {User} = require('../models/profiles')
+const { ipcMain } = require("electron");
+const { User } = require("../models/profiles");
 
-ipcMain.on('users-comunication', async (event, arg) => {
-  console.log({arg}); // prints "ping"
-  if (arg === 'ping') event.reply('asynchronous-reply', 'pong!');
-  if (arg.message === 'create') {
-    console.log('Hello World')
+ipcMain.on("users-comunication", async (event, arg) => {
+    console.log({ arg }); // prints "ping"
+    if (arg === "ping") event.reply("asynchronous-reply", "pong!");
+    if (arg.message === "create") {
+        console.log("Hello World");
 
-    const test = User.build({...arg.data})
-    await test.save()
+        const test = User.build({ ...arg.data });
+        await test.save();
 
-    event.reply('created-user', test)
-  }
-}); 
+        event.reply("created-user", test);
+    }
+});
