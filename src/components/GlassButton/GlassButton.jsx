038559e14a8e2d2cee735165bd7f2e095d51f622
riolp <riolp9991@gmail.com>
2022-08-18 01:22:39 -0400

    feat: add basic props to the button

--- src/components/GlassButton/GlassButton.jsx
+++ src/components/GlassButton/GlassButton.jsx
@@ -1,15 +1,22 @@
 import "./glass-button.css";
 
-const GlassButton = (props) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const GlassButton = ({
+  icon,
+  text = "Buttom",
+  fontSize = "1em",
+  margin = "0px 0px",
+}) => {
   return (
-    <div className="flat-button">
-      <a href="">
+    <div className="flat-button" style={{ margin: margin }}>
+      <button href="#" style={{ fontSize: fontSize }}>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-        Button
-      </a>
+        {text} <FontAwesomeIcon icon={icon} />
+      </button>
     </div>
   );
 };
--- src/features/Landing/Pages/LandingPage.jsx
+++ src/features/Landing/Pages/LandingPage.jsx
@@ -1,12 +1,29 @@
 import "./landing-page.css";
 
+import { faVideo, faCogs } from "@fortawesome/free-solid-svg-icons";
+
 import GlassButton from "../../../components/GlassButton/GlassButton";
 
 const LandingPage = (props) => {
   return (
-    <div>
-      <GlassButton />
-    </div>
+    <>
+      <div>
+        <GlassButton
+          fontSize="1.4em"
+          margin="10px 0px 0px 10px"
+          text="Peliculas"
+          icon={faVideo}
+        />
+      </div>
+      <div>
+        <GlassButton
+          margin="10px 0px 0px 10px"
+          fontSize="1.4em"
+          text="Configuracion"
+          icon={faCogs}
+        />
+      </div>
+    </>
   );
 };
 
