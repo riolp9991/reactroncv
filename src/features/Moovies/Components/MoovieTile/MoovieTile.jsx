6b0bccbdc45f718e9367bcf2731b8635e4a2dea2
riolp <riolp9991@gmail.com>
2022-08-18 18:04:49 -0400

    feat: add props to the tile

--- src/features/Moovies/Components/MoovieTile/MoovieTile.jsx
+++ src/features/Moovies/Components/MoovieTile/MoovieTile.jsx
@@ -1,10 +1,21 @@
 import "./MoovieTile.css";
 
-const MoovieTile = () => {
+import { useNavigate } from "react-router-dom";
+
+const MoovieTile = ({
+  text = "Nombre de la pelicula",
+  link = "/",
+  year = new Date().getFullYear(),
+}) => {
+  const navigate = useNavigate();
+  const redirect = () => {
+    navigate(link);
+  };
+
   return (
-    <div className="moovie-tile">
-      <span className="year">2021</span>
-      <h1>Nombre de la pelicula</h1>
+    <div className="moovie-tile" onClick={redirect}>
+      <span className="year">{year}</span>
+      <h1>{text}</h1>
     </div>
   );
 };
