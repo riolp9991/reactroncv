0913139f0f21cf1741d6260d7e174b54d130a49e
riolp <riolp9991@gmail.com>
2022-08-18 18:44:38 -0400

    fat: create the moovies database model

--- public/electron.js
+++ public/electron.js
@@ -11,48 +11,50 @@ const { connect: connectDB } = require("../src/database/database");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: 900,
-    height: 680,
-    webPreferences: {
-      contextIsolation: false,
-      nodeIntegration: true,
-    },
-  });
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+    mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+        width: 900,
+        height: 680,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+        },
+    });
+    mainWindow.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 //DATABASE MANAGEMENT
 
+const { Moovie } = require("../src/database/models/moovies");
 const { User } = require("../src/database/models/profiles");
 
 const forceSync = true;
 const sync = async () => {
-  await User.sync({ force: forceSync });
+    //await User.sync({ force: forceSync });
+    Moovie.sync({ force: forceSync });
 
-  console.log("Models sync");
+    console.log("Models sync");
 };
 
 connectDB();
---
+++ src/database/models/moovies.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../database");
+
+const Moovie = sequelize.define("Moovie", {
+    title: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    year: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    link: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = {
+    Moovie,
+};
