03c75e2028470e8a9c85f1025a42cc0a3e9c5a78
riolp <riolp9991@gmail.com>
2022-08-19 14:42:23 -0400

    feat: add view icon

--- src/features/Moovies/Components/MoovieTile/MoovieTile.jsx
+++ src/features/Moovies/Components/MoovieTile/MoovieTile.jsx
@@ -1,6 +1,8 @@
 import "./MoovieTile.css";
 
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const MoovieTile = ({
   text = "Nombre de la pelicula",
@@ -12,9 +14,17 @@ const MoovieTile = ({
     navigate(link);
   };
 
+  const openWebClick = (e) => {
+    e.stopPropagation();
+    console.log(`Clicked ${text} eye Icon`);
+  };
+
   return (
     <div className="moovie-tile" onClick={redirect}>
       <span className="year">{year}</span>
+      <span className="openWeb" onClick={openWebClick}>
+        <FontAwesomeIcon icon={faEye} />
+      </span>
       <h1>{text}</h1>
     </div>
   );
