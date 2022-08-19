b99ab5dceac81272eacfb93774f859d74442c226
riolp <riolp9991@gmail.com>
2022-08-19 15:34:10 -0400

    feat: add a reload button to the navBar

--- src/features/Moovies/Components/HomeNavBar/HomeNavBar.jsx
+++ src/features/Moovies/Components/HomeNavBar/HomeNavBar.jsx
@@ -1,27 +1,38 @@
 import "./HomeNavBar.css";
 
 import { FlatLink, FlatButton } from "../../../../components/FlatButton/index";
-import { faHome, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faSearch, faRedo } from "@fortawesome/free-solid-svg-icons";
 
 const HomeNavBar = (props) => {
   return (
     <nav className="moovies-home-nav">
-      <FlatLink
-        fontSize="0.9em"
-        to="/"
-        borderColor="var(--purple-accent)"
-        iconColor="var(--purple-text)"
-        text="Inicio"
-        icon={faHome}
-        reverseIcon={true}
-      />
-      <FlatButton
-        borderColor="var(--purple-accent)"
-        iconColor="var(--purple-text)"
-        fontSize="0.9em"
-        text="Filtrar"
-        icon={faSearch}
-      />
+      <div className="section">
+        <FlatLink
+          fontSize="0.9em"
+          to="/"
+          borderColor="var(--purple-accent)"
+          iconColor="var(--purple-text)"
+          text="Inicio"
+          icon={faHome}
+          reverseIcon={true}
+        />
+      </div>
+      <div className="section">
+        <FlatButton
+          borderColor="var(--purple-accent)"
+          iconColor="var(--purple-text)"
+          fontSize="0.9em"
+          text=""
+          icon={faRedo}
+        />
+        <FlatButton
+          borderColor="var(--purple-accent)"
+          iconColor="var(--purple-text)"
+          fontSize="0.9em"
+          text="filtrar"
+          icon={faSearch}
+        />
+      </div>
     </nav>
   );
 };
