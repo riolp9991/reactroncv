37ff6e9fc65e223185a1ca5b6f4047fc2cd9cff5
riolp <riolp9991@gmail.com>
2022-08-20 11:55:30 -0400

    bug: remove props to change for state management

--- src/features/Moovies/Components/MoovieFilter/MoovieFilter.jsx
+++ src/features/Moovies/Components/MoovieFilter/MoovieFilter.jsx
@@ -5,32 +5,22 @@ import FlatButton from "../../../../components/FlatButton/FlatButton";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-const MoovieFilter = ({ setTitleParent, setYearParent }) => {
+const MoovieFilter = ({}) => {
   const [titleVar, setTitle] = useState("");
   const [yearVar, setYear] = useState("");
-
-  const setTitleFunc = (value) => {
-    setTitle(value);
-    setTitleParent(value);
-  };
-
-  const setYearFunc = (value) => {
-    setYear(value);
-    setYearParent(value);
-  };
 
   return (
     <div className="moovie-filter">
       <div className="section">
         <CustomText
           value={titleVar}
-          setValue={setTitleFunc}
+          setValue={setTitle}
           width="100%"
           placeholder="Titulo"
         />
         <CustomText
           value={yearVar}
-          setValue={setYearFunc}
+          setValue={setYear}
           textAlign="center"
           type="number"
           width="260px"
--- src/features/Moovies/Pages/MooviesPage.jsx
+++ src/features/Moovies/Pages/MooviesPage.jsx
@@ -2,21 +2,10 @@ import HomeNavBar from "../Components/HomeNavBar/HomeNavBar";
 import MoovieList from "../Components/MoovieList/MoovieList";
 import MoovieFilter from "../Components/MoovieFilter/MoovieFilter";
 
-import { useState } from "react";
 const MooviesPage = ({}) => {
-  const [title, setTitle] = useState("");
-  const [year, setYear] = useState("");
-
-  let filter = (
-    <MoovieFilter
-      key="moovieFilter"
-      setTitleParent={setTitle}
-      setYearParent={setYear}
-    />
-  );
   return (
     <div>
-      <HomeNavBar key="MoovieNav" hidden={filter} />
+      <HomeNavBar hidden={<MoovieFilter />} />
       <MoovieList />
     </div>
   );
