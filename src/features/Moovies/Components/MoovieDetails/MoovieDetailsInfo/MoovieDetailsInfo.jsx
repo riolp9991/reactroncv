6599fbd38dc2996c06ae743d1077aae9a2f13a68
riolp <riolp9991@gmail.com>
2022-08-28 22:02:41 -0400

    feat: show the actors and the director s

--- src/features/Moovies/Components/MoovieDetails/MoovieDetailsInfo/MoovieDetailsInfo.jsx
+++ src/features/Moovies/Components/MoovieDetails/MoovieDetailsInfo/MoovieDetailsInfo.jsx
@@ -1,9 +1,36 @@
+import "./MoovieDetailsInfo.css";
+
 const MoovieDetailsInfo = ({ moovieData }) => {
+  let actors = [];
+  actors =
+    moovieData.actor.length > 10
+      ? moovieData.actor.slice(0, 10)
+      : moovieData.actor;
+
+  let directors = [];
+  directors = moovieData.director;
+
   return (
     <>
       <div className="moovie-sinopsis">
         <h1>Sinopsis</h1>
         <p>{moovieData.plot[0]}</p>
+      </div>
+      <div className="moovie-actors">
+        <h1>Actores</h1>
+        <div className="moovie-tags">
+          {actors.map((value, index) => {
+            return <span className="tag">{value.name}</span>;
+          })}
+        </div>
+      </div>
+      <div className="moovie-director">
+        <h1>Dirigido Por</h1>
+        <div className="moovie-tags">
+          {directors.map((value, index) => {
+            return <span className="tag">{value._}</span>;
+          })}
+        </div>
       </div>
     </>
   );
