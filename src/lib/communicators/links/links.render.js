76d93dad8a1498f9ae5c9b568d2adf075f43c779
riolp <riolp9991@gmail.com>
2022-08-30 21:18:03 -0400

    feat:

--- src/lib/communicators/links/links.render.js
+++ src/lib/communicators/links/links.render.js
@@ -2,25 +2,25 @@ const electron = window.require("electron");
 const { ipcRenderer } = electron;
 
 export const openLink = (link) => {
-  return new Promise((resolve) => {
-    ipcRenderer.once("linked", (_, args) => {
-      resolve(args);
+    return new Promise((resolve) => {
+        ipcRenderer.once("linked", (_, args) => {
+            resolve(args);
+        });
+        ipcRenderer.send("links-communication", {
+            message: "open",
+            url: link,
+        });
     });
-    ipcRenderer.send("links-communication", {
-      message: "open",
-      url: link,
-    });
-  });
 };
 
 export const getLinks = (link) => {
-  return new Promise((resolve) => {
-    ipcRenderer.once("links-fetched", (_, args) => {
-      resolve(args);
+    return new Promise((resolve) => {
+        ipcRenderer.once("links-fetched", (_, args) => {
+            resolve(args);
+        });
+        ipcRenderer.send("links-communication", {
+            message: "getLinks",
+            url: link,
+        });
     });
-    ipcRenderer.send("links-communication", {
-      message: "getLinks",
-      url: link,
-    });
-  });
 };
