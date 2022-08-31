d5ab70ca3fc3388a0b13115bc5daff6c2b0239c9
riolp <riolp9991@gmail.com>
2022-08-31 08:36:39 -0400

    feat: add icons to the links

--- src/components/WebFolders/WebFolders.jsx
+++ src/components/WebFolders/WebFolders.jsx
@@ -3,7 +3,15 @@ import { getLinks } from "./functions";
 import { useState, useEffect } from "react";
 import { FlatButton } from "../FlatButton/index.js";
 
-import { faDownload, faClipboard } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDownload,
+  faClipboard,
+  faFolder,
+  faFile,
+  faImage,
+  faFileVideo,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const WebFolders = ({ link = "", background = "var(--solid-bg)" }) => {
   const [currentLink, setCurrentLink] = useState(link);
@@ -61,10 +69,24 @@ const WebLink = ({
   },
   link = "",
 }) => {
+  console.log(text.split("."));
+  const isFolder = text.split(".")[0] == "" || text.split(".").length == 1;
+  console.log(isFolder);
+
+  let icon = faFile;
+
+  if (isFolder) icon = faFolder;
+  if (text.includes(".png") || text.includes(".jpg")) icon = faImage;
+  if (text.includes(".mp4") || text.includes(".avi") || text.includes(".mpg"))
+    icon = faFileVideo;
   return (
     <div onClick={onClick} className="web-folder-link">
       <div className="content">
-        <h1>{text}</h1>
+        <h1>
+          <FontAwesomeIcon icon={icon} />
+          &nbsp; &nbsp;
+          {text}
+        </h1>
         <div className="icons">
           <FlatButton font-size="1em" text="" icon={faClipboard} />
           <FlatButton font-size="1em" text="" icon={faDownload} />
